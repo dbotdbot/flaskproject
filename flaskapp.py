8c95bfab285e42239608acd146e9beb3f38ae1d9
Dylan Wheldale <dwheldale@gmail.com>
2019-05-25 12:28:33 +1000

    removed unsued methods

--- flaskapp.py
+++ flaskapp.py
@@ -38,24 +38,6 @@ def list():
    	print(row)
    return render_template("list.html",rows = rows)
 
-@app.route('/chartTest')         #generates test chart image
-def chartTest():
-	print("in chartTest")
-	fig = create_figure()
-	print("back in chartTest")
-	output = io.BytesIO()
-	FigureCanvas(fig).print_png(output)
-	return Response(output.getvalue(), mimetype='image/png')
-
-@app.route('/dispChart')         #displays page with test chart image
-def dispChartTest():
-	print("in DispChart")
-	return render_template('dispChart.html')
-
-@app.route('/dispTeaChart')      #displays page with tea consumption chart
-def dispTeaChart():
-	return render_template('dispTeaChart.html')
-
 @app.route('/testChart')
 def listCol():
 	print("in testChart")
@@ -79,7 +61,6 @@ def XYTestChart():
 	index = 0
 
 	print("Number of rows %2d" %(len(rows)))
-	
 
 	for row in rows:
 		tempstr1 = str(rows[index][0])
@@ -100,7 +81,6 @@ def create_figure_var(x, y):
 	fig = Figure()
 	fig.subplots_adjust(bottom=0.2)
 	axis = fig.add_subplot(1,1,1)
-	#axis2 = fig.add_subplot(1,1,1)
 	xs = x
 	ys = y
 	axis.set_xlabel('Time of Day')
@@ -119,55 +99,16 @@ def create_figure_var(x, y):
 	print("fig of type ")
 	print(type(fig))
 
-
 	axis.plot(xs, ys, 'o', color='black')
 	axis.plot(xs, ys, color='green')
 
-
 	return fig
 
-def create_figure():
-	#fig = Figure()
-	#axis = fig.add_subplot(1,1,1)
-	#xs = range(100)
-	#ys = [random.randint(1,50) for x in xs]
-	#axis.set_xlabel('x')
-	#axis.set_ylabel('y')
-	#fig.ylabel('y')
-	#axis.plot(xs, ys)
-
-	fig = plt.figure()
-	print("createed new fig with plt.figure()")
-	ax = fig.add_subplot(111)
-	print("added subplot")
-	ax.plot([1, 2, 3, 4], [10, 20, 25, 30], color='lightblue', linewidth=3)
-	print("created plot")
-	ax.scatter([0.3, 3.8, 1.2, 2.5], [11, 25, 9, 26], color='darkgreen', marker='^')
-	print("created scatter")
-	ax.set_xlim(0.5, 4.5)
-
-	print("in create_figure")
-	plt.close()   #attempt to fix server locking up
-	print("after plt.close")
-	plt.clf()
-	print("after plt.clf")
-	plt.cla()
-	print("after plt.cla")
-
-	return fig
 
 @app.route('/postjson', methods = ['POST'])
 def postJsonHandler():
 	print (request.is_json)
 	content = request.get_json()
-
-
-	#For testing
-	#print (content)
-	#print ()
-	#print (content['device'])
-	#print (content['value'])
-	#print (content['timestamp'])
 
 	#get content ready for sending to db
 	timestamp = datetime.datetime.now()
